bb825deba0f9b3b7ad42334726da6f617f64c8b5
harokb <harokb@gmail.com>
2014-01-25 17:37:35 +0000

    Moved InputPort class to Parse file and added some default symbols.

--- Symbol.py
+++ Symbol.py
@@ -1,6 +1,3 @@
-import re
-
-
 class Symbol(str): pass
 
 
@@ -12,32 +9,9 @@ def make_symbol(s, symbol_table={}):
 
 
 eof_object = Symbol('#<eof-object>')
+_quote, _if, _set, _define, _lambda, _begin, _definemacro, = map(make_symbol,
+                                                                 "quote   if   set!  define   lambda   begin   define-macro".split())
+
+_quasiquote, _unquote, _unquotesplicing = map(make_symbol, "quasiquote   unquote   unquote-splicing".split())
 
 
-class InputPort(object):
-    """An input port. Retains a line of chars."""
-    tokenizer = r'''\s*(,@|[('`,)]|"(?:[\\].|[^\\"])*"|;.*|[^\s('"`,;)]*)(.*)'''
-    matcher = re.compile(tokenizer)
-
-    def __init__(self, stream):
-        self.stream = stream
-        self.line = ''
-
-    def read_line(self):
-        try:
-            return self.stream.readline()
-        except:
-            return self.stream
-
-    def read_token(self):
-        """Return the next token, reading new text into line buffer if needed."""
-        while True:
-            if self.line == '':
-                self.line = self.read_line()
-
-            if self.line == '':
-                return eof_object
-
-            token, self.line = self.matcher.match(self.line).groups()
-            if token != '' and not token.startswith(';'):
-                return token
